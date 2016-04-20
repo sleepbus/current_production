6631dacab48b580afddb2969326024769e9e064a
David Lynam <dlynam@gmail.com>
2016-04-20 11:57:59 -0700

    check for availability on may 1st and 2nd

--- public/js/application.js
+++ public/js/application.js
@@ -111,7 +111,8 @@ var View = {
     var month = d.getMonth();
     if (month >= 4){ //show dates after may 3rd as available for now
       if (month == 4 && day < 3){
-        return false
+        var formatedDate = jQuery.datepicker.formatDate("yy-mm-dd", date);
+        return availableDates.indexOf(formatedDate) != -1;
       } else {
         return true;
       }
